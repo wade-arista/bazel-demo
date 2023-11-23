8f9f1d7efc89770a91a5edf18d681711d6a5e570
Wade Carpenter <wade@arista.com>
2023-11-23 12:45:00 -0800

    blackify test_dlopen.py

--- py_test_env/test_dlopen.py
+++ py_test_env/test_dlopen.py
@@ -3,16 +3,18 @@
 from ctypes import cdll
 import os
 from unittest import (
-   main,
-   TestCase,
+    main,
+    TestCase,
 )
 
+
 class TestMe(TestCase):
-   def testOne(self):
-      print(f"LD_LIBRARY_PATH={os.getenv('LD_LIBRARY_PATH')}")
-      libOne = cdll.LoadLibrary("libone.so")
-      func = getattr(libOne, "_Z3onev")
-      self.assertEqual(1, func())
+    def testOne(self):
+        print(f"LD_LIBRARY_PATH={os.getenv('LD_LIBRARY_PATH')}")
+        libOne = cdll.LoadLibrary("libone.so")
+        func = getattr(libOne, "_Z3onev")
+        self.assertEqual(1, func())
+
 
 if __name__ == "__main__":
-   main()
+    main()
