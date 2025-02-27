0241bf13ce66e2301fe621f17425983eb00d44ab
Wade Carpenter <wade@arista.com>
2025-02-27 14:13:25 -0800

    demonstration of "hdr dynamic deps" case

---
+++ mylib.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+#include "mylib.h"
+
+namespace MyLib {
+
+int myLibValue() {
+   return 42;
+}
+
+} // namespace MyLib
---
+++ mylib.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+#ifndef BAZEL_DEMO_MYLIB_H
+#define BAZEL_DEMO_MYLIB_H
+
+namespace MyLib {
+
+int myLibValue();
+
+} // namespace MyLib
+
+#endif // BAZEL_DEMO_MYLIB_H
