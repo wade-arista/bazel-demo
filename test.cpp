9f40bbea38a335f605b5cba0b3ec36c1c90db0f9
wwade <wwade@users.noreply.github.com>
2025-07-17 12:07:15 -0700

    demonstrate missing "interactive shell" with --experimental_use_hermetic_linux_sandbox

---
+++ test.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+int main(int argc, char **argv) {
+   return (argc > 1);
+}
